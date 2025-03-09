de70788f14da0476371e31012fba56f603c3e9f0
mangosherbet <88575595+mangosherbet@users.noreply.github.com>
2025-03-09 15:54:51 -0400

    Update wofi-windows.sh

--- wofi-windows.sh
+++ wofi-windows.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/bash
-hyprctl clients -j | jq -r '.[] | "\(.title) - \(.class) [\(.workspace.name)]"' | wofi --show dmenu | sed 's/.*\[//' | sed 's/\]//' | xargs -I{} hyprctl dispatch workspace {}
+
+# Check if wofi is already running
+if pgrep -x "wofi" > /dev/null; then
+    pkill -x "wofi"
+else
+    # List windows and switch to the selected workspace
+    hyprctl clients -j | jq -r '.[] | "\(.title) - \(.class) [\(.workspace.name)]"' | \
+    wofi --show dmenu | \
+    sed 's/.*\[//' | sed 's/\]//' | \
+    xargs -I{} hyprctl dispatch workspace {}
+fi
 
